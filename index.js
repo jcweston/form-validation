7335db3283964af712ba61e3926a4ec5791f7eba
James Weston <j.c.weston@outlook.com>
2020-05-28 17:01:59 +0100

    set up validation for first name, last name, and date of birth

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
+const submitButton=document.getElementById("submit-button")
+const user = {
+    'firstName':'',
+    'lastName':'',
+    'dob':'',
+    'userName':'',
+    'gender':'',
+    'email':'',
+    'phoneNumber':''
+}
+let valid=true
 
+function testName(nameType) {
+    let nameRegex=/[\D]/
+    let testType = `${nameType}Name`
+    let name=document.getElementById(testType)
+    let regexTest = nameRegex.test(name.value)
+    if (!regexTest) {
+        name.className="input-error"
+        valid=false
+    } else {
+        user[testType]=name.value
+        name.className="input-normal"
+    }
+}
+
+function testDate() {
+    let date=document.getElementById("dob")
+    let currentDate=new Date()
+    let dateNum=date.valueAsNumber
+    if (dateNum>currentDate-568080000000) {
+        date.className="input-error"
+        valid=false
+    } else {
+        user['dob']=date.value
+        date.className="input-normal"
+    }
+}
+
+submitButton.addEventListener("click",()=>{
+    valid=true
+    testName('first')
+    testName('last')
+    testDate()
+    if(valid) {
+       console.log(user) 
+    }
+})
